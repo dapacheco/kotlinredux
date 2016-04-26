d54d311a996dd543551e596e557235bcdc5db271
David Pacheco <dapacheco+github@gmail.com>
2016-04-26 22:11:53 +0100

    Ensure activity subscribes on resume and un-subscribes  on pause from the store.

--- app/src/main/kotlin/com/davepacheco/kotlinredux/MainActivity.kt
+++ app/src/main/kotlin/com/davepacheco/kotlinredux/MainActivity.kt
@@ -10,16 +10,30 @@ import org.jetbrains.anko.onClick
 class MainActivity : AppCompatActivity() {
 
     val store : BasicStore<Int, String> = BasicStore(::counter, 0)
+    lateinit var unsubscribeFromStore: () -> Unit
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setUpUI()
+
+    }
+
+    private fun setUpUI() {
         counter_text.text = "Counter is ${store.state}"
 
         add_button.onClick { store.dispatch("INCREMENT") }
         remove_button.onClick { store.dispatch("DECREMENT") }
+    }
 
-        store.subscribe({ state : Int -> counter_text.text = "Counter is ${store.state}" })
+    override fun onResume() {
+        super.onResume()
+        unsubscribeFromStore = store.subscribe({ state : Int -> counter_text.text = "Counter is ${store.state}" })
+    }
+
+    override fun onPause() {
+        unsubscribeFromStore()
+        super.onPause()
     }
 }
