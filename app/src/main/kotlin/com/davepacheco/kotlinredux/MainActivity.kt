90aa4a97422ea2424a6361e194b80c1b2b6399a1
David Pacheco <dapacheco+github@gmail.com>
2017-03-13 21:39:05 +0000

    Update to Kotlin 1.1

--- app/src/main/kotlin/com/davepacheco/kotlinredux/MainActivity.kt
+++ app/src/main/kotlin/com/davepacheco/kotlinredux/MainActivity.kt
@@ -3,7 +3,6 @@ package com.davepacheco.kotlinredux
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.davepacheco.kotlinredux.databinding.ActivityMainBinding
 import com.davepacheco.kotlinredux.redux.BasicStore
 import com.davepacheco.kotlinredux.redux.counter
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,22 +10,22 @@ import org.jetbrains.anko.onClick
 
 class MainActivity : AppCompatActivity() {
 
-    val store : BasicStore<Int, String> = BasicStore(::counter, 0)
+    val store: BasicStore<Int, String> = BasicStore(::counter, 0)
     lateinit var unsubscribeFromStore: () -> Unit
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main);
-        binding.counter = store.state
+//        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main);
+//        binding.counter = store.state
 
         setUpUI()
 
     }
 
     private fun setUpUI() {
-//        counter_text.text = "Counter is ${store.state}"
+        counter_text.text = "Counter is ${store.state}"
 
         add_button.onClick { store.dispatch("INCREMENT") }
         remove_button.onClick { store.dispatch("DECREMENT") }
@@ -34,7 +33,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        unsubscribeFromStore = store.subscribe({ state : Int -> counter_text.text = "Counter is ${store.state}" })
+        unsubscribeFromStore = store.subscribe({ state: Int -> counter_text.text = "Counter is ${store.state}" })
     }
 
     override fun onPause() {
